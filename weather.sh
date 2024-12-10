1676df1251c3fe5e363a86e3c1b83326e913c4e4
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2024-12-10 18:42:15 +0000

    Tidying Repository

--- weather.sh
+++ weather.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+# This script is for running daily weather reports from the Athenry Station
 wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
+
+# Laura Brogan 20/12/24
